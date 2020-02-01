75bac52e84d0a246c754c25767fa15c9ac8e0e8e
anthonymachniak <anthony.machniak@gmail.com>
2020-02-01 01:17:57 -0500

    Added Persistent Bugger exercise.

--- PersistentBugger/Program.cs
+++ PersistentBugger/Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace PersistentBugger
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
+        //public static int Main(long baseNumber)
         {
-            Console.WriteLine("Hello World!");
+            long baseNumber = long.Parse(args[0]);
+            int persistence = 0;
+
+            while(baseNumber.ToString().Length != 1)
+            {
+                List<int> multiplicationList = new List<int>();
+                foreach(var digit in baseNumber.ToString())
+                {
+                    multiplicationList.Add(int.Parse(digit.ToString()));
+                }
+
+                var result = 1;
+                foreach(var value in multiplicationList)
+                {
+                    result *= value;
+                }
+
+                baseNumber = result;
+                persistence++;
+            }
+
+            Console.WriteLine(persistence);
         }
     }
 }
