febcfbb63655cdde525ab2ef3214326c9c31ce0d
anthonymachniak <anthony.machniak@gmail.com>
2020-02-01 00:32:16 -0500

    Add project files.

---
+++ DuplicateEncoder/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DuplicateEncoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var letterCount = new Dictionary<char, int>();
+
+            args[0] = args[0].ToLower();
+            foreach(var letter in args[0])
+            {
+                if(letterCount.ContainsKey(letter))
+                {
+                    letterCount[letter]++;
+                }
+                else
+                {
+                    letterCount.Add(letter, 1);
+                }
+            }
+
+            var alteredWord = string.Empty;
+            foreach(var letter in args[0])
+            {
+                if(letterCount[letter] > 1)
+                {
+                    alteredWord += ")";
+                }
+                else
+                {
+                    alteredWord += "(";
+                }
+            }
+
+            Console.WriteLine(alteredWord);
+        }
+    }
+}
---
+++ PlayingWithDigits/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace PlayingWithDigits
+{
+    class Program
+    {
+        static long Main(int baseNumber, int exponent)
+        {
+            double result = 0;
+
+            foreach(var digit in baseNumber.ToString())
+            {
+                result += Math.Pow(int.Parse(digit.ToString()), exponent);
+                exponent++;
+            }
+
+            if(result % baseNumber == 0)
+            {
+                double factor = result / baseNumber;
+
+                //Console.WriteLine((string)(factor + " * " + result + " = " + baseNumber));
+                return (int) factor;
+            }
+
+            //Console.WriteLine(-1);
+            return -1;
+        }
+    }
+}
